03030725b1c9b255d5affb3a932ce0a1acd6897d
James Kaupert <jrkaupert@gmail.com>
2017-02-14 21:14:48 -0500

    gulp and getting server running

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+
+gulp.task('serveprod', function() {
+  connect.server({
+    root: [__dirname],
+    port: process.env.PORT || 5000,
+    livereload: false
+  });
+});
+
+gulp.task('default',['serveprod']);
